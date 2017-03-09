ae66dc87cb6c56fa13551aa56167d5fbbda5cf27
Rae Farine <rfarine1027@gmail.com>
2017-03-08 19:51:36 -0500

    add back button and home button to result page

--- src/components/result/result.jsx
+++ src/components/result/result.jsx
@@ -1,9 +1,14 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { go, goBack } from 'react-router-redux';
 import { getResultById } from 'redux/selectors';
 import style from './result.scss';
 
-const Result = ({ result }) => {
+const Result = ({
+  back,
+  home,
+  result,
+}) => {
   return (
     <div className={style.component}>
       <img
@@ -12,12 +17,32 @@ const Result = ({ result }) => {
         alt=""
       />
       <div className={style.meta}>
-        <strong>Rating:</strong> {result.rating} <br />
+        <strong>Rating:</strong> <span className={style.rating}>{result.rating}</span>
+        <br />
         <strong>Date/Time Uploaded:</strong> {result.dateTime} <br />
-        <strong>User:</strong> {result.userName}
+        {
+          result.userName &&
+            <strong>User:</strong> `${result.userName}`
+        }
+      </div>
+      <div className={style.buttonContainer}>
+        <button className={style.homeButton} onClick={home}>Home</button>
+        <button onClick={back}>Go Back</button>
       </div>
     </div>
   );
+};
+
+
+Result.propTypes = {
+  back: PropTypes.func.isRequired,
+  home: PropTypes.func.isRequired,
+  result: PropTypes.shape({
+    dateTime: PropTypes.string.isRequired,
+    looping: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
+    userName: PropTypes.string,
+  }).isRequired,
 };
 
 const mapStateToProps = (state, props) => {
@@ -26,13 +51,15 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-Result.propTypes = {
-  result: PropTypes.shape({
-    dateTime: PropTypes.string.isRequired,
-    looping: PropTypes.string.isRequired,
-    rating: PropTypes.string.isRequired,
-    userName: PropTypes.string.isRequired,
-  }).isRequired,
+const mapDispatchToProps = (dispatch) => {
+  return {
+    back() {
+      dispatch(goBack());
+    },
+    home () {
+      dispatch(go(-2));
+    },
+  };
 };
 
-export default connect(mapStateToProps)(Result);
+export default connect(mapStateToProps, mapDispatchToProps)(Result);
